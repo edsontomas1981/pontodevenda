68cc16a632a13016586857b310f4ff948445a3ef
edsontomas1981 <edsontomasdev@gmail.com>
2024-09-08 15:05:59 -0300

    asa

--- js/configuracoes/btnSalvaConfiguracao.js
+++ js/configuracoes/btnSalvaConfiguracao.js
@@ -1,11 +1,43 @@
-import Swal from 'sweetalert2/dist/sweetalert2.js'
-import 'sweetalert2/src/sweetalert2.scss'
+// Caminho para o fs do Node.js só funciona no Electron, então você precisa usar o Electron para isso
+const fs = require('fs');
+const path = require('path');
 
-const btnSalvaConfiguracoes = ()=>{
-	Swal.fire({
-		title: 'Error!',
-		text: 'Do you want to continue',
-		icon: 'error',
-		confirmButtonText: 'Cool'
-	})
+// Tornando a função global
+btnSalvaConfiguracoes = function() {
+  const configPath = path.join(__dirname, '../../configuracoes.json');
+
+  // Carrega o arquivo de configurações existente
+  fs.readFile(configPath, 'utf8', (err, data) => {
+    if (err) {
+		  alert('Erro ao carregar o arquivo de configurações:');		
+      console.error('Erro ao carregar o arquivo de configurações:', err);
+      return;
+    }else{
+		  alert('carregou')
+	}
+
+    // Converte o JSON para um objeto JavaScript
+    let configuracoes = JSON.parse(data);
+
+    // Atualiza as configurações com novos valores (exemplo de como capturar novas configs)
+    const novasConfiguracoes = {
+      tema: {
+        corPrimaria: "#ff0000" // Aqui você pode pegar os valores dos inputs, por exemplo
+      }
+    };
+
+    configuracoes = {
+      ...configuracoes,
+      ...novasConfiguracoes
+    };
+
+    // Escreve as novas configurações no arquivo
+    fs.writeFile(configPath, JSON.stringify(configuracoes, null, 2), 'utf8', (err) => {
+      if (err) {
+        console.error('Erro ao salvar o arquivo de configurações:', err);
+        return;
+      }
+      console.log('Configurações salvas com sucesso!');
+    });
+  });
 }
--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-// main.js
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 const path = require('path');
+
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
+        width: 1100,
+        height: 700,
         autoHideMenuBar: true,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
             enableRemoteModule: false,
             nodeIntegration: false
-          },
+        },
     });
+
     const indexPath = path.join(__dirname, 'windows', 'index.html');
     mainWindow.loadFile(indexPath).catch(err => console.error("Erro ao carregar o arquivo HTML:", err));
+
+    globalShortcut.register('Ctrl+Shift+I', () => {
+        mainWindow.webContents.openDevTools();
+    });
+
+
 }
 
 app.on('ready', createWindow);
-
-console.log('Isso é um log no renderizador');
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -33,3 +38,6 @@ app.on('activate', () => {
         createWindow();
     }
 });
+
+
+
